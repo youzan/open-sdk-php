24a428a923e906a621a9ec5683765f648ed44c47
xu42 <xu42.cn@gmail.com>
2019-10-31 10:58:12 +0800

    support decryptWithoutDecodeMessage

--- src/Open/Helper/CryptoHelper.php
+++ src/Open/Helper/CryptoHelper.php
@@ -27,5 +27,24 @@ class CryptoHelper
             CryptoConfig::IV
         );
     }
+
+
+    /**
+     * 消息解密(订购消息等) 不对消息做decode
+     *
+     * @param $messages         string  接收到的消息密文
+     * @param $clientSecret     string  应用client_secret
+     * @return string 解密后文本 (Json串)
+     */
+    public static function decryptWithoutDecodeMessage($messages, $clientSecret)
+    {
+        return openssl_decrypt(
+            $messages,
+            CryptoConfig::AES_128_CBC,
+            substr($clientSecret, 0, CryptoConfig::KEY_LENGTH),
+            CryptoConfig::OPTIONS,
+            CryptoConfig::IV
+        );
+    }
 }
 
