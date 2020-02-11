738c69c572535b4608d58259e29440708dae66f9
xu42 <xu42.cn@gmail.com>
2020-02-11 14:57:23 +0800

    for qa test

--- src/Open/Config/HttpConfig.php
+++ src/Open/Config/HttpConfig.php
@@ -7,13 +7,13 @@ namespace Youzan\Open\Config;
 class HttpConfig
 {
 
-    const REQUEST_URL = 'https://open.youzanyun.com/api/%s/%s?access_token=%s';
+    const REQUEST_URL = 'http://bifrost-gateway.qa.s.qima-inc.com/api/%s/%s?access_token=%s';
 
-    const REQUEST_URL_AUTH_EXEMPT = 'https://open.youzanyun.com/api/auth_exempt/%s/%s';
+    const REQUEST_URL_AUTH_EXEMPT = 'http://bifrost-gateway.qa.s.qima-inc.com/api/auth_exempt/%s/%s';
 
-    const REQUEST_URL_TEXTAREA = 'https://open.youzanyun.com/api/_textarea_/%s/%s?access_token=%s';
+    const REQUEST_URL_TEXTAREA = 'http://bifrost-gateway.qa.s.qima-inc.com/api/_textarea_/%s/%s?access_token=%s';
 
-    const REQUEST_TOKEN_URL = 'https://open.youzanyun.com/auth/token';
+    const REQUEST_TOKEN_URL = 'http://bifrost-oauth.qa.s.qima-inc.com/auth/token';
 
 
     private static $formatUserAgent = 'YZY-Open-Client %s-PHP';
