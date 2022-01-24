f04ce53553ae2924de51d1b087cb76b4eae8e199
likai_lk <m18736955651@163.com>
2022-01-24 20:12:11 +0800

    日志打印

--- src/Open/Security/HttpSecretInvoker.php
+++ src/Open/Security/HttpSecretInvoker.php
@@ -26,6 +26,7 @@ class HttpSecretInvoker
 
     public function invoke($param) {
        $response = HttpsClient::postJson($this->initUrl,$param);
+       error_log($response,"/usr/local/errors.log");
        $mapper = new DataSecurityJsonMapper();
        return $mapper->map(json_decode($response),new PlainResult());
     }
